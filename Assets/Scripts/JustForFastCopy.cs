2083e0888c87afff97cbe95ba47d0513ebe408e6
hww <hww@sina.com>
2023-03-24 18:16:04 +0800

    Update

---
+++ Assets/Scripts/JustForFastCopy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JustForFastCopy : MonoBehaviour
+{
+    public SkinnedMeshRenderer origin;
+    public SkinnedMeshRenderer target;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
